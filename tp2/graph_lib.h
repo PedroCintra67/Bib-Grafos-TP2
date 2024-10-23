b5848e1bb80829ad3f2f206b15db39121c330026
drbarros2004 <134460119+drbarros2004@users.noreply.github.com>
2024-10-23 18:10:49 -0300

    Create graph_lib.h

---
+++ tp2/graph_lib.h
@@ -0,0 +1,72 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <fstream>
+using namespace std;
+
+// Funções de receber entrada
+
+vector<vector<bool>> txt_to_adjacency_matrix(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
+vector<vector<int>> txt_to_adjacency_vector(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
+
+// Funções para obter informações básicas do grafo (vetor de adjacência):
+
+int minDegree(const vector<vector<int>>& grafo);
+int maxDegree(const vector<vector<int>>& grafo);
+double averageDegree(const vector<vector<int>>& grafo);
+double medianDegree(const vector<vector<int>>& grafo);
+
+// Funções para percorrer o grafo (vetor de adjacência):
+
+void BFS_tree_vector(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreBFS.txt");
+void DFS_tree_vector(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt");
+
+void Distancia_vector(const vector<vector<int>>& graph, int origem, int destino);
+void Diametro_aproximado_vector(const vector<vector<int>>& graph);
+void Diametro_vector(const vector<vector<int>>& graph);
+
+
+// Funções para percorrer o grafo (matriz de adjacência):
+
+void DFS_tree_matrix(const vector<vector<bool>>& matrix, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt");
+void BFS_tree_matrix(const vector<vector<bool>>& matrix, int origem, string nome_do_arquivo_de_saida = "arvoreBFS.txt");
+
+void Distancia_matrix(const vector<vector<bool>>& matrix, int origem, int destino);
+void Diametro_aproximado_matrix(const vector<vector<bool>>& matrix);
+void Diametro_matrix(const vector<vector<bool>>& matrix);
+
+
+// Contabilizando o tempo:
+
+double DFS_tree_vector_with_execution_time(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt");
+double BFS_tree_vector_with_execution_time(const vector<vector<int>>& graph, int origem, string nome_do_arquivo_de_saida = "arvoreBFS.txt");
+
+double DFS_tree_matrix_with_execution_time(const vector<vector<bool>>& matrix, int origem, string nome_do_arquivo_de_saida = "arvoreDFS.txt");
+double BFS_tree_matrix_with_execution_time(const vector<vector<bool>>& matrix, int origem, string nome_do_arquivo_de_saida = "arvoreBFS.txt");
+
+
+// TP2:
+
+vector<vector<float>> txt_to_weight_adjacency_matrix(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
+
+vector<vector<pair<int, float>>> txt_to_weight_adjacency_vector(const string& nome_arquivo, string nome_do_arquivo_de_saida_principal = "resultados.txt");
+
+
+vector<float> Dijkstra_Vector_Vector(const vector<vector<pair<int,float>>>& graph, int origem);
+vector<float> Dijkstra_Vector_Heap(const vector<vector<pair<int,float>>>& graph, int origem);
+
+vector<float> Dijkstra_Matrix_Heap(const vector<vector<float>>& matrix, int origem);
+vector<float> Dijkstra_Matrix_Vector(const vector<vector<float>>& matrix, int origem);
+void Caminho_Minimo_Vector(const vector<vector<pair<int, float>>>& graph, int origem, int destino);
+void Caminho_Minimo_Matrix(const vector<vector<float>>& matrix, int origem, int destino);
+
+double Dijkstra_Vector_Heap_With_Execution_Time(const vector<vector<pair<int,float>>>& graph, int origem);
+
+double Dijkstra_Vector_Vector_With_Execution_Time(const vector<vector<pair<int,float>>>& graph, int origem);
+
+double Dijkstra_Matrix_Vector_With_Execution_Time(const vector<vector<float>>& matrix, int origem);
+
+double Dijkstra_Matrix_Heap_With_Execution_Time(const vector<vector<float>>& matrix, int origem);
+
+#endif
